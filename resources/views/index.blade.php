3f10208152a00a468f8d5dc4cc39cc4700373fc1
Verry <verryrwibawa@students.amikom.ac.id>
2022-12-18 20:43:46 +0700

    create basic view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.main')
+ 
+@section('container')
+    <!-- Hero -->
+    <section>
+    @include('partials.navbar')
+        <div class="container">
+            <h1>Hero</h1>
+        </div>
+    </section>
+
+    <!-- Corporate Training -->
+    <section>
+        <div class="container">
+            <h1>Corporate Training</h1>
+        </div>
+    </section>
+
+    <!-- Program Kami -->
+    <section>
+        <div class="container">
+            <h1>Program Kami</h1>
+        </div>
+    </section>
+
+    <!-- Benefits -->
+    <section>
+        <div class="container">
+            <h1>Benefits</h1>
+        </div>
+    </section>
+
+    <!-- Clients -->
+    <section>
+        <div class="container">
+            <h1>Clients</h1>
+        </div>
+    </section>
+
+    <!-- Testimoni -->
+    <section>
+        <div class="container">
+            <h1>Testimoni</h1>
+        </div>
+    </section>
+
+    <!-- Konsultasi -->
+    <section>
+        <div class="container">
+            <h1>Konsultasi</h1>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <section>
+        <div class="container">
+            <h1>Footer</h1>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MOA Corporate Training | {{ $title }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+        <!-- Styles --> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    </head>
+    <body>
+        <div>
+            @yield('container')
+        </div>
+        
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
